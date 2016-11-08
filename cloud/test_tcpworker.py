27447a2d2ecebbe39b30f1d3c8934a43377425f8
Eric Lee <ericlee0803@yahoo.com>
2016-11-08 15:51:04 -0500

    Fixed mistake in test_tcpworker.py

--- cloud/test_tcpworker.py
+++ cloud/test_tcpworker.py
@@ -1,6 +1,13 @@
 from poap.tcpserve import SimpleSocketWorker
 import argparse
 
+def f(x):
+    logging.info("Request for {0}".format(x))
+    if TIMEOUT > 0:
+        time.sleep(TIMEOUT)
+    logging.info("OK, done")
+    return (x-1.23)*(x-1.23)
+
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('integers', metavar='N', type=int, nargs='+',)
 args = parser.parse_args()
@@ -8,4 +15,4 @@ port = args.integers
 port = port[0]
 print "Launching worker on port ", port
 name = ('localhost', int(port))
-SimpleSocketWorker(sockname=name, retries=1).run()
+SimpleSocketWorker(f, sockname=name, retries=1).run()
