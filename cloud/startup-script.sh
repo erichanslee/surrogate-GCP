ea06082093f685e3b06c4769473d2ba9020e027c
Sebastian Ament <Basti@dhcp-rhodes-629.eduroam.cornell.edu>
2016-11-09 18:03:10 -0500

    start up file

--- cloud/startup-script.sh
+++ cloud/startup-script.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# for host:
+touch startup_script.sh
+echo "echo 'startup script executed.'" >> startup_script.sh
+export IP=$(curl http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip --header "Metadata-Flavor: Google")
+sed -i '1i\'"export IP=$IP" startup_script.sh
+
+export HOSTNAME=$(curl http://metadata.google.internal/computeMetadata/v1/instance/hostname --header "Metadata-Flavor: Google")
+sed -i '1i\'"export IP=$IP" startup_script.sh
+
 # [START startup_script]
 
 # [END startup_script]
