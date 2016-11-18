87a3f0504d31ed52ab3db2e4934c0641eb2c12d1
Eric Lee <ericlee0803@yahoo.com>
2016-11-18 17:25:35 -0500

    Reverted test_tcpserver.py to old version... accidentally messed it up

--- cloud/test_tcpserver.py
+++ cloud/test_tcpserver.py
@@ -3,16 +3,12 @@ from poap.tcpserve import ThreadedTCPServer
 from poap.strategy import FixedSampleStrategy
 
 
-strategy = FixedSampleStrategy([])
+strategy = FixedSampleStrategy([1, 2, 3, 4, 5])
 server = ThreadedTCPServer(sockname=('0.0.0.0', 0) , strategy=strategy)
-tstrategy = InputStrategy(server.controller, strategy);
-server.strategy = tstrategy
 name = server.sockname 
 print(name)
 cthread = threading.Thread(target=server.run)
 cthread.start()
-for k in range(0, 6):
-	tstrategy.eval(k)
 cthread.join()
 result = server.controller.best_point()
 print("Final: {0:.3e} @ {1}".format(result.value, result.params))
