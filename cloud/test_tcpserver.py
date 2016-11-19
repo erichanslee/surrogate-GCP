7bc88f35a65240351ff12aa831ab09f8d5933d06
Eric Lee <ericlee0803@yahoo.com>
2016-11-19 21:26:07 +0000

    Added GP properly

--- cloud/test_tcpserver.py
+++ cloud/test_tcpserver.py
@@ -1,17 +1,36 @@
 import threading
+import GPy
+import numpy as np
 from poap.tcpserve import ThreadedTCPServer
 from poap.strategy import FixedSampleStrategy
 from poap.strategy import InputStrategy
 
-strategy = FixedSampleStrategy([1, 2, 3, 4, 5])
+# Start Up TCP Server
+strategy = FixedSampleStrategy([])
 server = ThreadedTCPServer(sockname=('0.0.0.0', 0))
 tstrategy = InputStrategy(server.controller, strategy)
 server.strategy = tstrategy
-tstrategy.eval(6)
+
+# Evaluate points
+X = np.linspace(-3, 3, num=50)
+for x in X:
+	tstrategy.eval(x)
+
+# Get Server Socket
 name = server.sockname 
 print(name)
+
+# Start Server
 cthread = threading.Thread(target=server.run)
 cthread.start()
 cthread.join()
+offset = 10;
+Xnew = np.zeros([len(server.controller.fevals)-offset, 1])
+Ynew = np.zeros([len(server.controller.fevals)-offset, 1])
+for k in range(len(server.controller.fevals)-offset):
+	Ynew[k] = server.controller.fevals[k].value
+	Xnew[k] = server.controller.fevals[k].params
+kernel = GPy.kern.RBF(input_dim=1, variance=1., lengthscale=1.)
+m = GPy.models.GPRegression(Xnew,Ynew,kernel)
 result = server.controller.best_point()
 print("Final: {0:.3e} @ {1}".format(result.value, result.params))
