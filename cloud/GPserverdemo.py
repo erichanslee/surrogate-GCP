0880ed5fb6457fc35317f8da7e6aefb71309b281
Eric Lee <ericlee0803@yahoo.com>
2016-12-01 01:24:47 +0000

    Working GPserverdemo

--- cloud/GPserverdemo.py
+++ cloud/GPserverdemo.py
@@ -10,7 +10,7 @@ from poap.strategy import InputStrategy
 
 # Evaluate points in initial sweep
 numevals = 20
-X = np.linspace(-3, 3, num=numevals)
+X = 2*np.random.random(numevals)-1
 
 # Start Up TCP Server
 strategy = FixedSampleStrategy(X)
@@ -23,24 +23,38 @@ print "Starting Server on socket ", name
 # Start CloudUnit Worker
 unit = cloudUnit('xenon-marker-147522')
 unit.startWorker()
-print "Worker Successfully Started"
+print "Worker Successfully Started..."
 
 # Start Server
 cthread = threading.Thread(target=server.run)
 cthread.start()
-cthread.join()
-Xnew = np.zeros([len(server.controller.fevals), 1])
-Ynew = np.zeros([len(server.controller.fevals), 1])
-for k in range(len(server.controller.fevals)):
-	Ynew[k] = server.controller.fevals[k].value
-	Xnew[k] = server.controller.fevals[k].params
-kernel = GPy.kern.RBF(input_dim=1, variance=1., lengthscale=1.)
+time.sleep(35)
 plt.ion()
 plt.show()
-for i in range(2,len(Xnew)):
-	m = GPy.models.GPRegression(Xnew[0:i],Ynew[0:i],kernel)
-	fig = m.plot()
-	plt.pause(.5)
+while(len(server.controller.fevals) < numevals):
+	try:
+		time.sleep(1.5)
+		Xnew = np.zeros([len(server.controller.fevals), 1])
+		Ynew = np.zeros([len(server.controller.fevals), 1])
+		for k in range(len(server.controller.fevals)):
+			Ynew[k] = server.controller.fevals[k].value
+			Xnew[k] = server.controller.fevals[k].params
+		idx = np.isnan(Ynew)
+		idx = np.nonzero(idx)
+		Xnew = np.delete(Xnew, idx)
+		Ynew = np.delete(Ynew, idx)
+		Ynew = np.reshape(Ynew, [len(Ynew), 1])
+		Xnew = np.reshape(Xnew, [len(Xnew), 1])
+		print Xnew
+		print Ynew
+		kernel = GPy.kern.RBF(input_dim=1, variance=1., lengthscale=1.)
+		m = GPy.models.GPRegression(Xnew,Ynew,kernel)
+		fig = m.plot()
+		plt.pause(.5)
+#		m.optimize_restarts(3)
+	except:
+		print('Waiting...')
+cthread.join()
 result = server.controller.best_point()
 unit.cleanupWorkers()
 print("Final: {0:.3e} @ {1}".format(result.value, result.params))
